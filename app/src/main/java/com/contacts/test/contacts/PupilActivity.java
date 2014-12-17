e5b2b0ac6f443a1a94d3a2b6c81d3c697c702ac4
Niraj Kumar Chauhan <niraj.1991@gmail.com>
2014-12-17 18:28:25 +0530

    Updated final

--- app/src/main/java/com/contacts/test/contacts/PupilActivity.java
+++ app/src/main/java/com/contacts/test/contacts/PupilActivity.java
@@ -2,30 +2,48 @@ package com.contacts.test.contacts;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
+
+import com.contacts.test.util.Pupil;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by CHAUHAN on 12/16/14.
- */
 public class PupilActivity extends Activity {
-    Spinner testPassed,lessonHad;
-    Button back;
+    boolean isFBFriend,isPassed,isBooked;
+    EditText fullName, houseStreet, town, postcode, contact;
+    RadioButton isFacebookFriend, isTheoryPassed, isTestBooked;
+    Spinner testPassed, lessonHad;
+    Button back, addPupil, clearData;
     List<String> lessonHadArray;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_pupil);
+        fullName = (EditText) findViewById(R.id.txtFullName);
+        houseStreet = (EditText) findViewById(R.id.txtHouseStreet);
+        town = (EditText) findViewById(R.id.txtTown);
+        postcode = (EditText) findViewById(R.id.txtPostcode);
+        contact = (EditText) findViewById(R.id.txtContactNumber);
         testPassed = (Spinner) findViewById(R.id.testPassed);
-        lessonHad =(Spinner) findViewById(R.id.lessonsHad);
-        //back button
+        lessonHad = (Spinner) findViewById(R.id.lessonsHad);
+        isFacebookFriend = (RadioButton) findViewById(R.id.radiobtnfacebook);
+        isTheoryPassed = (RadioButton) findViewById(R.id.radiobtnTheoryPassed);
+        isTestBooked = (RadioButton) findViewById(R.id.radiobtnTestBooked);
+
+        /**
+         * back button
+         */
         back = (Button) findViewById(R.id.backButton);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,16 +51,26 @@ public class PupilActivity extends Activity {
                 finish();
             }
         });
-        //end of back button
 
+        /**
+         * Add Pupil Button
+         */
+        addPupil = (Button) findViewById(R.id.btnSave);
+        addPupil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Pupil object = new Pupil();
+                object.save();
+            }
+        });
         lessonHadArray = new ArrayList<String>();
-        for(int i=0 ;i<=100;i++){
-            lessonHadArray.add(""+i);
+        for (int i = 0; i <= 100; i++) {
+            lessonHadArray.add("" + i);
         }
-        ArrayAdapter<CharSequence> testPassedAdapter = ArrayAdapter.createFromResource(this,R.array.testPassed,android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> testPassedAdapter = ArrayAdapter.createFromResource(this, R.array.testPassed, android.R.layout.simple_spinner_item);
         testPassedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         testPassed.setAdapter(testPassedAdapter);
-        ArrayAdapter<String> lessonHadAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,lessonHadArray);
+        ArrayAdapter<String> lessonHadAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lessonHadArray);
         lessonHadAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         lessonHad.setAdapter(lessonHadAdapter);
     }
--- app/src/main/java/com/contacts/test/util/Pupil.java
+++ app/src/main/java/com/contacts/test/util/Pupil.java
@@ -2,9 +2,125 @@ package com.contacts.test.util;
 
 import com.orm.SugarRecord;
 
-/**
- * Created by nirajkumar.chauhan on 12/17/2014.
- */
+
+import java.util.Date;
+
 public class Pupil extends SugarRecord<Pupil> {
 
+    String name;
+    String houseStreet;
+    String town;
+    String postcode;
+    String contact;
+    Boolean isFacebookFriend;
+    Boolean isTheoryPassed;
+    Boolean isTestBooked;
+    Date testBookedDate;
+    String testPassed;
+    String lessonHad;
+
+    public Pupil() {
+    }
+
+    public Pupil(String name, String houseStreet, String town, String postcode, String contact, Boolean isFacebookFriend, Boolean isTheoryPassed, Boolean isTestBooked, Date testBookedDate, String testPassed, String lessonHad) {
+        this.name = name;
+        this.houseStreet = houseStreet;
+        this.town = town;
+        this.postcode = postcode;
+        this.contact = contact;
+        this.isFacebookFriend = isFacebookFriend;
+        this.isTheoryPassed = isTheoryPassed;
+        this.isTestBooked = isTestBooked;
+        this.testBookedDate = testBookedDate;
+        this.testPassed = testPassed;
+        this.lessonHad = lessonHad;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHouseStreet() {
+        return houseStreet;
+    }
+
+    public void setHouseStreet(String houseStreet) {
+        this.houseStreet = houseStreet;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public Boolean getIsFacebookFriend() {
+        return isFacebookFriend;
+    }
+
+    public void setIsFacebookFriend(Boolean isFacebookFriend) {
+        this.isFacebookFriend = isFacebookFriend;
+    }
+
+    public Boolean getIsTheoryPassed() {
+        return isTheoryPassed;
+    }
+
+    public void setIsTheoryPassed(Boolean isTheoryPassed) {
+        this.isTheoryPassed = isTheoryPassed;
+    }
+
+    public Boolean getIsTestBooked() {
+        return isTestBooked;
+    }
+
+    public void setIsTestBooked(Boolean isTestBooked) {
+        this.isTestBooked = isTestBooked;
+    }
+
+    public Date getTestBookedDate() {
+        return testBookedDate;
+    }
+
+    public void setTestBookedDate(Date testBookedDate) {
+        this.testBookedDate = testBookedDate;
+    }
+
+    public String getTestPassed() {
+        return testPassed;
+    }
+
+    public void setTestPassed(String testPassed) {
+        this.testPassed = testPassed;
+    }
+
+    public String getLessonHad() {
+        return lessonHad;
+    }
+
+    public void setLessonHad(String lessonHad) {
+        this.lessonHad = lessonHad;
+    }
 }
