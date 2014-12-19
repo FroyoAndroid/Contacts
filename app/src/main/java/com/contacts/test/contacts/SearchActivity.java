e38d81c5d0872b2088e5719365f1f3f0e185e17c
Niraj Kumar Chauhan <niraj.1991@gmail.com>
2014-12-19 19:15:01 +0530

    latest code

--- app/src/main/java/com/contacts/test/contacts/SearchActivity.java
+++ app/src/main/java/com/contacts/test/contacts/SearchActivity.java
@@ -3,15 +3,25 @@ package com.contacts.test.contacts;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import java.util.List;
+import com.contacts.test.util.Pupil;
 /**
  * Created by nirajkumar.chauhan on 12/18/2014.
  */
 public class SearchActivity extends Activity {
-
+    List<Pupil> pupil;
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.search_pupil);
+        try{
+         pupil = Pupil.findWithQuery(Pupil.class, "Select * from Pupil","");
+        }catch(Exception e){
+            Toast.makeText(this.getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+            Log.d("database",pupil.toString());
+        }
     }
 }
