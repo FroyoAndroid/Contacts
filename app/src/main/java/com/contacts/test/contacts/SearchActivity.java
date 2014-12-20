595161c7aa4d64418755e26f71a5a138adfb1139
NIRAJ KUMAR CHAUHAN <niraj.1991@gmail.com>
2014-12-20 12:12:31 +0530

    list and add pupil code updated

--- app/src/main/java/com/contacts/test/contacts/SearchActivity.java
+++ app/src/main/java/com/contacts/test/contacts/SearchActivity.java
@@ -3,15 +3,39 @@ package com.contacts.test.contacts;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import com.contacts.test.util.Pupil;
+import com.contacts.test.util.PupilListAdapter;
+
+import java.util.List;
 
 /**
  * Created by nirajkumar.chauhan on 12/18/2014.
  */
 public class SearchActivity extends Activity {
+    SearchView searchView;
+    ListView pupilList;
+    List<Pupil> pupil;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.search_pupil);
+        pupilList = (ListView)findViewById(R.id.pupilList);
+        searchView =(SearchView) findViewById(R.id.searchPupil);
+
+        pupil = Pupil.listAll(Pupil.class);
+        Log.d("Database",pupil.size()+"");
+
+        PupilListAdapter adapter = new PupilListAdapter(this,R.layout.pupil_list_item,pupil);
+        pupilList.setAdapter(adapter);
     }
+
+
 }
---
+++ app/src/main/java/com/contacts/test/util/PupilListAdapter.java
@@ -0,0 +1,46 @@
+package com.contacts.test.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.contacts.test.contacts.R;
+
+import java.util.List;
+
+/**
+ * Created by CHAUHAN on 12/20/14.
+ */
+public class PupilListAdapter extends ArrayAdapter<Pupil>{
+
+    Context context;
+    List<Pupil> pupil;;
+    int layoutResourceID;
+
+    public PupilListAdapter(Context context, int resource, List<Pupil> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.pupil = objects;
+        this.layoutResourceID = resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+       // return super.getView(position, convertView, parent);
+        if(convertView == null) {
+            //inflate the layout
+            LayoutInflater infaltor = ((Activity) context).getLayoutInflater();
+            convertView = infaltor.inflate(layoutResourceID, parent, false);
+        }
+            Pupil obj =pupil.get(position);
+            TextView textView = (TextView)convertView.findViewById(R.id.listitemTextView);
+            textView.setText(obj.getName());
+            return convertView;
+        //}return super.getView(position, convertView, parent);
+    }
+}
