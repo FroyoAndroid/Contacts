837b54f7c9def9d6c07cf7ed28d9cdede4d9c09f
Niraj Kumar Chauhan <niraj.1991@gmail.com>
2014-12-22 11:48:22 +0530

    Search Pupil Code Updated

--- app/src/main/java/com/contacts/test/contacts/SearchActivity.java
+++ app/src/main/java/com/contacts/test/contacts/SearchActivity.java
@@ -3,24 +3,30 @@ package com.contacts.test.contacts;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SearchView;
 
 import com.contacts.test.util.Pupil;
 import com.contacts.test.util.PupilListAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
+
 import com.contacts.test.util.Pupil;
+
 /**
  * Created by nirajkumar.chauhan on 12/18/2014.
  */
 public class SearchActivity extends Activity {
 
-    SearchView searchView;
+    EditText searchView;
     ListView pupilList;
     List<Pupil> pupil;
 
@@ -28,14 +34,41 @@ public class SearchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_pupil);
-        pupilList = (ListView)findViewById(R.id.pupilList);
-        searchView =(SearchView) findViewById(R.id.searchPupil);
+        pupilList = (ListView) findViewById(R.id.pupilList);
+        searchView = (EditText) findViewById(R.id.searchPupil);
 
         pupil = Pupil.listAll(Pupil.class);
-        Log.d("Database",pupil.size()+"");
+        Log.d("Database", pupil.size() + "");
 
-        PupilListAdapter adapter = new PupilListAdapter(this,R.layout.pupil_list_item,pupil);
+        final PupilListAdapter adapter = new PupilListAdapter(this, R.layout.pupil_list_item, pupil);
         pupilList.setAdapter(adapter);
+
+        searchView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int textlength = s.length();
+                List<Pupil> tempArrayList = new ArrayList<Pupil>();
+                for (Pupil p : pupil) {
+                    if (textlength <= p.getName().length()) {
+                        if (p.getName().toLowerCase().contains(s.toString().toLowerCase())) {
+                            tempArrayList.add(p);
+                        }
+                    }
+                }
+                final PupilListAdapter mAdapter = new PupilListAdapter(SearchActivity.this, R.layout.pupil_list_item, tempArrayList);
+                pupilList.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
     }
 
