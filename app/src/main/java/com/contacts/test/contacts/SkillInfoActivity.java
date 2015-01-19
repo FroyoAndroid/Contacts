b9a1fa9e4337b3f8a1a3981ef2f79fb220df667d
Niraj Kumar Chauhan <niraj.1991@gmail.com>
2015-01-19 18:35:41 +0530

    Skill Info

---
+++ app/src/main/java/com/contacts/test/contacts/SkillInfoActivity.java
@@ -0,0 +1,56 @@
+package com.contacts.test.contacts;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.contacts.test.util.SkillInfoAdapter;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+/**
+ * Created by nirajkumar.chauhan on 1/19/2015.
+ */
+public class SkillInfoActivity extends Activity {
+
+    ArrayList<String> linkedHashMapValues;
+    LinkedHashMap<String, String> hashMap;
+    String[] skills;
+    Button backButton;
+    ListView skillListView;
+    SkillInfoAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skill_info);
+        backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        skillListView = (ListView) findViewById(R.id.skillInfoListView);
+        skills = new String[]{"cockpitDrill", "movingAway", "stopping", "turningLeft", "turningRight", "tJunction", "crossRoads", "roundAbouts", "pedestarianCrossing", "emergencyStop", "usingMirrors", "dualCarriageways", "oneWayStreets", "meetingSituations", "defensiveDriving", "ecoDriving", "turnInTheRoads", "parallelParking", "leftReverse", "bayParking", "independentDriving", "mockTests"};
+        hashMap = new LinkedHashMap<String, String>();
+        for (int i = 0; i < skills.length; i++) {
+            hashMap.put(skills[i], getStringResourceByName(skills[i]));
+        }
+
+        /*Log.d("Size",  getStringResourceByName("cockpitDrill"));*/
+        adapter = new SkillInfoAdapter(hashMap,SkillInfoActivity.this);
+        skillListView.setAdapter(adapter);
+
+    }
+
+    private String getStringResourceByName(String aString) {
+        String packageName = getPackageName();
+        int resId = getResources().getIdentifier(aString, "string", packageName);
+        return getString(resId);
+    }
+}
