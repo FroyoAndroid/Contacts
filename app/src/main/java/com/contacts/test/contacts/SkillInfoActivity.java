eb79750df677923f8e9afebbdd63496965fcd8d4
Niraj Kumar Chauhan <niraj.1991@gmail.com>
2015-01-21 00:08:13 +0530

    skill activity updated with the test passed and lesson had spinners

--- app/src/main/java/com/contacts/test/contacts/SkillInfoActivity.java
+++ app/src/main/java/com/contacts/test/contacts/SkillInfoActivity.java
@@ -1,10 +1,15 @@
 package com.contacts.test.contacts;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.contacts.test.util.SkillInfoAdapter;
@@ -23,11 +28,12 @@ public class SkillInfoActivity extends Activity {
     Button backButton;
     ListView skillListView;
     SkillInfoAdapter adapter;
-
+    Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skill_info);
+        mContext = this;
         backButton = (Button) findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,9 +49,32 @@ public class SkillInfoActivity extends Activity {
             hashMap.put(skills[i], getStringResourceByName(skills[i].toString()));
         }
 
-        Log.d("Size",  ""+hashMap.size());
+        //Log.d("Size",""+hashMap.size());
         adapter = new SkillInfoAdapter(hashMap,this);
         skillListView.setAdapter(adapter);
+
+        //Skill ListView Item Click Listener
+        skillListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("Competency Info",hashMap.get(skills[position]));
+                final Dialog dialog = new Dialog(mContext);
+                dialog.setTitle("Description...");
+
+                //setting the custom dialog component
+                EditText skillInfoDetailText = (EditText)dialog.findViewById(R.id.skillInfoDetail);
+                skillInfoDetailText.setText(hashMap.get(skills[position]),null);
+                ImageView skilImage = (ImageView) dialog.findViewById(R.id.skillInfoImage);
+                Button dialogClose = (Button) dialog.findViewById(R.id.dialogClose);
+                dialogClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
 
     }
 
